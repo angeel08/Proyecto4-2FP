de7bfc16597e0c7f16668ed38a9c728bcb463065
bedez159 <bedez00123@gmail.com>
2026-01-26 11:00:25 +0100

    nuevo ejericio de borrar un registro

--- python/acces/BD/bd.delete.py
+++ python/acces/BD/bd.delete.py
@@ -1,5 +1,7 @@
 import pyodbc
 
+#EJERCICIO DE ÁNGEL BLÁZQUEZ JIMÉNEZ
+
 #db_file = r'C:\Users\Gigabyte\Desktop\proyectos\Proyecto4 - clase 2º\python\acces\BD\Base_datos\empresa.accdb' #en casa
 db_file = r'C:\Users\angel.blajim\Proyecto4-2FP\python\acces\BD\base_datos\empresa.accdb' #en clase
 
