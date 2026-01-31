72572edea730dca98f3e017b0b0bfd17d4605cdc
Angel <angblajim12@gmail.com>
2026-01-31 13:48:09 +0100

    .

--- python/acces/BD/bd_actualizar.py
+++ python/acces/BD/bd_actualizar.py
@@ -1,7 +1,7 @@
 import pyodbc
 
 db_file = r'C:\Users\Gigabyte\Desktop\proyectos\Proyecto4 - clase 2º\python\acces\BD\Base_datos\empresa.accdb' #en casa
-db_file = r'C:\Users\angel.blajim\Proyecto4-2FP\python\acces\BD\base_datos\empresa.accdb' #en clase
+#db_file = r'C:\Users\angel.blajim\Proyecto4-2FP\python\acces\BD\base_datos\empresa.accdb' #en clase
 
 conn_str = (
     r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'
@@ -17,11 +17,13 @@ ID_buscar = int(input("Dime tu id: "))
 #ver toda la informacion del usuario
 cursor.execute("SELECT * FROM clientes WHERE id=?", (ID_buscar))
 cliente = cursor.fetchone()
+actua = cursor.fetchone()
 
 def actualizado():
-    cursor.execute(f"SELECT * FROM clientes WHERE id={ID_buscar}")
-    actuzalizar = cursor.fetchone(actuzalizar)
-    return actuzalizar
+    
+    #cursor.execute(f"SELECT * FROM clientes WHERE id={ID_buscar}")
+    #actuzalizar = cursor.fetchone(actuzalizar)
+    #return actuzalizar
 
 #funcion de mostrar los datos del cliente
 def mostrar(cliente):
@@ -43,7 +45,7 @@ if cliente:
 
     cursor.execute("SELECT * FROM clientes WHERE id=?", (ID_buscar))
 
-    actualizado()
+    actualizado(cliente)
     #mostrar(cliente) #error sin resolver => no se actualiza los datos guardados
 else:
     print("EROR......")
